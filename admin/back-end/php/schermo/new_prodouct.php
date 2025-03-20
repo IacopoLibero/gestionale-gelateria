c81db17675a0185916178160390d7cb19df3d476
pclab <iacopoliberolavoro@gmail.com>
2025-03-20 17:05:57 +0100

    try

--- admin/back-end/php/schermo/new_prodouct.php
+++ admin/back-end/php/schermo/new_prodouct.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 
-require_once '../../../../../../connessione.php';
+require_once '../../../../../../../../../connessione.php';
 
 // Check if form was submitted
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
