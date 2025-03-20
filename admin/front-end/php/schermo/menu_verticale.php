04ab4eae7514bef670b6808f3a56e4124825c4f9
macchina_kali <iacopoliberolavoro@gmail.com>
2025-03-21 00:34:50 +0100

    inizio della pagina del menu tv

--- admin/front-end/php/schermo/menu_verticale.php
+++ admin/front-end/php/schermo/menu_verticale.php
@@ -7,8 +7,18 @@
   <link rel="stylesheet" href="../../../front-end/css/schermo/menu_verticale.css">
 </head>
 <body>
-  
-  <script src="../../../js/dashboard.js"></script>
+  <div class="background-container"></div>
+  <div class="content">
+    <img src="../../../../img/david/david_logo_testo_2.png" alt="logodavid" class="logodavid">
+
+    <div class="divider-with-text">
+      <hr><img src="../../../../img/tipologie/mini_gelato.png" class="gelatino">Gelati / Ice Creams<img class="gelatino" src="../../../../img/tipologie/mini_gelato.png"><hr>
+    </div>
+
+    <div>
+      
+    </div>
+  </div>
   <script src="../../../js/schermo/menu_verticale.js"></script>
 </body>
 </html>
