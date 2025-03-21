d9c139d3265cfc035a997cb5070a18c46f32e111
pclab <iacopoliberolavoro@gmail.com>
2025-03-21 15:44:23 +0100

    fix percorsi connessione

--- admin/front-end/php/schermo/edit_prodotto.php
+++ admin/front-end/php/schermo/edit_prodotto.php
@@ -8,7 +8,7 @@ if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
 }
 
 // Include database connection
-require_once '../../../connessione.php';
+require_once '../../../../connessione.php';
 
 // Initialize variables
 $error = '';
