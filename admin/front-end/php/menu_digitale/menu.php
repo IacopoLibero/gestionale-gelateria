8a3c1f9d03466c8bb93c2593cdc78137ae17cbbb
macchina_kali <iacopoliberolavoro@gmail.com>
2025-04-11 15:31:29 +0200

    Add margin-top to menu row for improved spacing

--- admin/front-end/php/menu_digitale/menu.php
+++ admin/front-end/php/menu_digitale/menu.php
@@ -212,7 +212,7 @@ $conn->close();
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </head>
 <body class="container-fluid">
-    <div class="row">
+    <div class="row" style="margin-top: 20px;">
         <?php echo $menuHTML; ?>
     </div>
 </body>
